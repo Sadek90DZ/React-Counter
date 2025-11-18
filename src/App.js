7b1f1636b82934a5e647233d061a2913f813112f
Sadek90DZ <sadek90dz@gmail.com>
2025-11-18 16:52:49 +0100

    Add background with its opacity binded to the counter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import { useState } from "react";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Counter from "./components/Counter";
 function App() {
+  const [opacity, setOpacity] = useState(0.5);
+  const changeOpacity = (opa) => {
+    setOpacity(opa);
+  };
+
   return (
-    <div className="container">
+    <div
+      className="container"
+      style={{ backgroundColor: "rgba(240,240,240," + opacity + ")" }}
+    >
       <Header />
-      <Counter />
+      <Counter onChangeOpacity={changeOpacity} />
       <Footer />
     </div>
   );
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,8 +1,9 @@
 import { useState } from "react";
 
-const Counter = () => {
+const Counter = (props) => {
   //Deconstructe the useState function.
   const [count, setCount] = useState(0);
+  var opacity = 0.5;
   //Store my audio file on a variable.
   const myAudio = new Audio("click.wav");
 
@@ -13,17 +14,22 @@ const Counter = () => {
     //Check if it's an increase or a decrease action.
     if (name === "inc") {
       setCount(count + 1); //incriment by 1.
+      opacity = opacity - (count / 200).toFixed(2);
       //Reset the count to 0 if it surpasses the 99.
       if (count >= 100) {
         setCount(0);
+        opacity = 0.5;
       }
     } else {
       setCount(count - 1); //decriment by 1.
+      opacity = opacity - (count / 200).toFixed(2);
       //Reset the count to 0 if it goes below -99.
       if (count <= -100) {
         setCount(0);
+        opacity = 0.5;
       }
     }
+    props.onChangeOpacity(opacity);
     myAudio.play(); //Play the audio file stored on myAudio
   };
   return (
