c25e76db118cec73fa56530d352b10799c8e5b57
Sadek90DZ <sadek90dz@gmail.com>
2025-11-18 14:38:29 +0100

    Add Comments

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,23 +1,30 @@
 import { useState } from "react";
 
 const Counter = () => {
+  //Deconstructe the useState function.
   const [count, setCount] = useState(0);
+  //Store my audio file on a variable.
   const myAudio = new Audio("click.wav");
 
+  //Handeling the click on +, - buttons.
   const handleClick = (e) => {
+    //Deconstructe the name propriety from e.target.
     const { name } = e.target;
+    //Check if it's an increase or a decrease action.
     if (name === "inc") {
-      setCount(count + 1);
+      setCount(count + 1); //incriment by 1.
+      //Reset the count to 0 if it surpasses the 99.
       if (count >= 100) {
         setCount(0);
       }
     } else {
-      setCount(count - 1);
+      setCount(count - 1); //decriment by 1.
+      //Reset the count to 0 if it goes below -99.
       if (count <= -100) {
         setCount(0);
       }
     }
-    myAudio.play();
+    myAudio.play(); //Play the audio file stored on myAudio
   };
   return (
     <main>
